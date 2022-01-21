ee16859365baedba1525489624bd4e07d68be578
lowwwe <lowep@itcarlow.ie>
2022-01-21 09:43:50 +0000

    first rectangle

---
+++ newSimon/newSimon/Game.cpp
@@ -0,0 +1,138 @@
+/// <summary>
+/// @author Peter Lowe
+/// @date May 2019
+///
+/// you need to change the above lines or lose marks
+/// </summary>
+
+#include "Game.h"
+#include <iostream>
+
+
+
+/// <summary>
+/// default constructor
+/// setup the window properties
+/// load and setup the text 
+/// load and setup thne image
+/// </summary>
+Game::Game() :
+	m_window{ sf::VideoMode{ 800U, 600U, 32U }, "SFML Game" },
+	m_buttonRed{ sf::Vector2f{200.0f,200.0f} },
+	m_exitGame{false} //when true game will exit
+
+{
+	setupFontAndText(); // load font 
+	setupButtons();
+}
+
+/// <summary>
+/// default destructor we didn't dynamically allocate anything
+/// so we don't need to free it, but mthod needs to be here
+/// </summary>
+Game::~Game()
+{
+}
+
+
+/// <summary>
+/// main game loop
+/// update 60 times per second,
+/// process update as often as possible and at least 60 times per second
+/// draw as often as possible but only updates are on time
+/// if updates run slow then don't render frames
+/// </summary>
+void Game::run()
+{	
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	const float fps{ 60.0f };
+	sf::Time timePerFrame = sf::seconds(1.0f / fps); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame); //60 fps
+		}
+		render(); // as many as possible
+	}
+}
+/// <summary>
+/// handle user and system events/ input
+/// get key presses/ mouse moves etc. from OS
+/// and user :: Don't do game update here
+/// </summary>
+void Game::processEvents()
+{
+	sf::Event newEvent;
+	while (m_window.pollEvent(newEvent))
+	{
+		if ( sf::Event::Closed == newEvent.type) // window message
+		{
+			m_exitGame = true;
+		}
+		if (sf::Event::KeyPressed == newEvent.type) //user pressed a key
+		{
+			processKeys(newEvent);
+		}
+	}
+}
+
+
+/// <summary>
+/// deal with key presses from the user
+/// </summary>
+/// <param name="t_event">key press event</param>
+void Game::processKeys(sf::Event t_event)
+{
+	if (sf::Keyboard::Escape == t_event.key.code)
+	{
+		m_exitGame = true;
+	}
+}
+
+/// <summary>
+/// Update the game world
+/// </summary>
+/// <param name="t_deltaTime">time interval per frame</param>
+void Game::update(sf::Time t_deltaTime)
+{
+	if (m_exitGame)
+	{
+		m_window.close();
+	}
+}
+
+/// <summary>
+/// draw the frame and then switch buffers
+/// </summary>
+void Game::render()
+{
+	m_window.clear(sf::Color::Black);
+	m_window.draw(m_buttonRed);
+	m_window.display();
+}
+
+void Game::setupButtons()
+{
+	m_buttonRed.setFillColor(RED);
+	m_buttonRed.setPosition(sf::Vector2f(570.0f, 30.0f));
+}
+
+/// <summary>
+/// load the font and setup the text message for screen
+/// </summary>
+void Game::setupFontAndText()
+{
+	if (!m_ArialBlackfont.loadFromFile("ASSETS\\FONTS\\ariblk.ttf"))
+	{
+		std::cout << "problem loading arial black font" << std::endl;
+	}
+	
+
+}
+
---
+++ newSimon/newSimon/Game.h
@@ -0,0 +1,44 @@
+/// <summary>
+/// author Pete Lowe May 2019
+/// you need to change the above line or lose marks
+/// </summary>
+#ifndef GAME_HPP
+#define GAME_HPP
+/// <summary>
+/// include guards used so we don't process this file twice
+/// same as #pragma once
+/// Don't forget the endif at the bottom
+/// </summary>
+#include <SFML/Graphics.hpp>
+
+const sf::Color RED{ 180,0,0,255 };
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	/// <summary>
+	/// main method for game
+	/// </summary>
+	void run();
+
+private:
+
+	void processEvents();
+	void processKeys(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render();
+	void setupButtons();
+	void setupFontAndText();
+
+
+	sf::RenderWindow m_window; // main SFML window
+	sf::Font m_ArialBlackfont; // font used by message
+	sf::RectangleShape m_buttonRed;// red button
+	bool m_exitGame; // control exiting game
+
+};
+
+#endif // !GAME_HPP
+
